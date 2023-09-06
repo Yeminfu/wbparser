be0efd3354f61beeb4002ed7e689f83a5f245c96
Yeminfu <yeminfu4319@gmail.com>
2023-09-06 13:27:19 +1000

    create client

---
+++ client.js
@@ -0,0 +1,59 @@
+
+require('dotenv').config();
+
+(async () => {
+    const { categoriesFromDB: categories } = await fetch(
+        `${process.env.url}/api/categories/get`,
+        {
+            method: "POST"
+        }
+    )
+        .then(x => x.json());
+    // console.log('categories', Object.keys(categories));
+
+
+    category_loop: for (let categoryIndex = 0; categoryIndex < categories.length; categoryIndex++) {
+        const category = categories[categoryIndex];
+
+        page_looop: for (let page = 1; page < 2000; page++) {
+
+            const productsFromWB = await ParseCategoryPage(
+                category.link,
+                page,
+                category.name
+            )
+            if (productsFromWB.products?.length) {// если есть товары
+                // newStore.push(...log);
+                // setLog(`страница: ${page}, получили товаров: ${productsFromWB.products?.length}, создано товаров в бд: ${productsFromWB.created} категория: ${category.name}`);
+                console.log(`страница: ${page}, получили товаров: ${productsFromWB.products?.length}, создано товаров в бд: ${productsFromWB.created} категория: (${categoryIndex})/${category.name}`);
+            } else {
+                startPage = 1;
+                continue category_loop;
+            }
+        }
+
+    }
+    console.log('собрали все товары с категории');
+
+
+})();
+
+console.log(process.env.url) // remove this after you've confirmed it is working
+
+
+async function ParseCategoryPage(link, page, category_name) {
+    return await fetch(
+        `${process.env.url}/api/categories/parse`,
+        {
+            method: "POST",
+            body: JSON.stringify({
+                link: `${link}?sort=popular&page=${page}`,
+                category_name: category_name,
+            })
+        }
+    )
+        .then(x => x.json())
+        .then(x => {
+            return x;
+        });
+}
